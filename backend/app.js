a8aca2fb9876850c9d5650b0e82a5987f4f4ed87
Sami <samisamman26@yahoo.com>
2023-11-24 15:55:26 -0800

    finished phase 2

--- backend/app.js
+++ backend/app.js
@@ -34,8 +34,48 @@ app.use(
 )
 
 // ROUTES
-// backend/app.js
 app.use(routes); // Connect all the routes
+
+// PROCESS RESOURCE ERRORS
+app.use((req, res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err)
+});
+
+const { ValidationError } = require('sequelize');
+
+// PROCESS SEQUELIZE ERRORS
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+            let errors = {};
+            for (let error of err.errors) {
+            errors[error.path] = error.message;
+            }
+        err.title = 'Validation error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+
+// ERROR FORMATTER
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+        });
+    }
+);
+
+
 
 
 module.exports = app;
