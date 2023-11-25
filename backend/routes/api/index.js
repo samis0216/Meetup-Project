18bcf78b46ca717dcbb07f5bb50e1e01cc1eb7db
Sami <samisamman26@yahoo.com>
2023-11-24 16:58:07 -0800

    fix commit

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body});
-});
+router.use(restoreUser);
 
 module.exports = router
