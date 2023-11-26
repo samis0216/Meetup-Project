ddc2c1ee9e2c1e09cc5cafe12852a7651f7c79d1
Sami <samisamman26@yahoo.com>
2023-11-26 15:18:20 -0800

    added firstname lastname migrations

---
+++ backend/db/migrations/20231126231502-added-firstname-lastname.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { query } = require('express');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.schema;
+  options.tableName = 'Users'
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(options, 'firstName', {
+      type: Sequelize.STRING
+    })
+
+    await queryInterface.addColumn(options, 'lastName', {
+      type: Sequelize.STRING
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn(options, 'firstName')
+    await queryInterface.removeColumn(options, 'lastName')
+  }
+};
