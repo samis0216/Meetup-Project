753d298de86ca614b3acd96348e90a5c29be0e50
Sami <samisamman26@yahoo.com>
2023-11-28 15:58:19 -0800

    created empty groups router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const { restoreUser } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const usersRouter = require('./users.js');
 const sessionRouter = require('./session.js');
+const groupsRouter = require('./groups.js')
 const user = require('../../db/models/user.js');
 
 router.use(restoreUser);
