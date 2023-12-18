e96936a98ac56e8f82c6f4c97925dedad25d0e25
Sami <samisamman26@yahoo.com>
2023-12-18 12:27:33 -0800

    fixed small skeleton mistakes, added redirect to nav logo

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -9,13 +9,13 @@ export default function Homepage () {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus molestie nunc non. Lectus vestibulum mattis ullamcorper velit. Nascetur ridiculus mus mauris vitae ultricies leo. Quisque id diam vel quam elementum pulvinar etiam non quam. Nam at lectus urna duis convallis convallis tellus id interdum. Accumsan sit amet nulla facilisi morbi tempus.</p>
                 </div>
                 <div className='img-container'>
-                    <img src="https://placehold.co/325x325" />
+                    <img id='first-img' src="https://placehold.co/325x325" />
                 </div>
             </div>
             <div className='second-body-container'>
                 <div className='how-meetup'>
                     <h3>How Meetup Works</h3>
-                    <p class='how-meetup-text'>
+                    <p className='how-meetup-text'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                 </div>
@@ -37,7 +37,7 @@ export default function Homepage () {
                     <p>Something for creating group</p>
                 </div>
             </div>
-            <a href="" className='join-meetup'>Join Meetup</a>
+            <a href="/" className='join-meetup'>Join Meetup</a>
         </div>
     )
 }
--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -5,14 +5,20 @@ import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import LoginFormModal from '../LoginFormModal/LoginFormModal';
 import SignupFormModal from '../SignupFormModal/SIgnupFormModal';
 import './Navigation.css';
+import { useNavigate } from 'react-router-dom'
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
+    const navigate = useNavigate()
+
+    const redirect = () => {
+        navigate('/')
+    }
 
     return (
         <div>
             <div className='header'>
-                <img src="https://assets.stickpng.com/images/584c57c01fc21103bb375bae.png" />
+                <img src="https://assets.stickpng.com/images/584c57c01fc21103bb375bae.png" id='meetup-logo' onClick={redirect}/>
                 <ul>
                     <li>
                         <NavLink exact to="/">Home</NavLink>
